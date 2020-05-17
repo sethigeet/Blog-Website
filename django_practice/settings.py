4272ae44a7fa35a7fbc61e9ed90861aeffaa5c1a
Geet Sethi <geetsethi05@gmail.com>
2020-05-17 17:08:17 +0530

    Added Static URL to settings file

--- django_practice/settings.py
+++ django_practice/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 # for production
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
